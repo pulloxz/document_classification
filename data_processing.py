3af55f6607b4896a836a112267c057ece44b4a0e
Fatima Alaa Muhammed <fatimaalaa2001xx@gmail.com>
2023-11-23 01:52:17 +0300

    edit commit

--- data_processing.py
+++ data_processing.py
@@ -1,32 +1,29 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
 import os
-#data set should be provided
 
-#dataset directories
-dataset_directories = []
+# dataset https://www.kaggle.com/datasets/jensenbaxter/10dataset-text-document-classification/
+dataset_directories = ['dataset/sport', 'dataset/business', 'dataset/entertainment', 'dataset/food',
+                       'dataset/technology', 'dataset/space', 'dataset/politics', 'dataset/medical',
+                       'dataset/historical', 'dataset/graphics']
 
-#list to store processed data from all datasets
 all_document_texts = []
 custom_stopwords_file = 'stopword'
 
-# Reading custom stopwords once
 with open(custom_stopwords_file, 'r') as stopword_file:
     custom_stopwords = set(stopword_file.read().splitlines())
 
-# Create a TfidfVectorizer
 tfidf = TfidfVectorizer(lowercase=True)
 
-# The remove_custom_stopwords function
+
 def remove_custom_stopwords(text):
     words = text.split()
     filtered_words = [word for word in words if word not in custom_stopwords]
     return ' '.join(filtered_words)
 
-# Process and accumulate data from all datasets
+
 for data_directory in dataset_directories:
     document_texts = []
-
     for filename in os.listdir(data_directory):
         if filename.endswith('.txt'):
             file_path = os.path.join(data_directory, filename)
@@ -34,14 +31,19 @@ for data_directory in dataset_directories:
                 text = file.read()
                 document_texts.append(text)
 
-    # Preprocess the text data
     document_texts = [remove_custom_stopwords(text) for text in document_texts]
 
-    # Append the preprocessed text data to the list
     all_document_texts.extend(document_texts)
 
-# Create TF-IDF features for the accumulated text data
 tfidf_result = tfidf.fit_transform(all_document_texts)
 
-# Save the tfidf_result to a file using joblib
-joblib.dump(tfidf_result, 'tfidf_result.pkl')
+joblib.dump(tfidf, 'tfidf_vectorizer.pkl')
+joblib.dump(tfidf.vocabulary_, 'tfidf_vocabulary.pkl')
+
+# # Print the vocabulary, if needed
+# print('\nindexes numbers:')
+# print(tfidf.vocabulary_)
+#
+# # Print the TF-IDF values, if needed
+# print('\nTF-IDF values:')
+# print(tfidf_result)
--- model.py
+++ model.py
@@ -5,8 +5,11 @@ import joblib
 import numpy as np
 
 tfidf_result = joblib.load('tfidf_result.pkl')
+
 labels = np.array([0] * 100 + [1] * 100 + [2] * 100 + [3] * 100 + [4] * 100 + [5] * 100 + [6] * 100 + [7] * 100 + [8] * 100 + [9] * 100)
+
 X_train, X_test, y_train, y_test = train_test_split(tfidf_result, labels, test_size=0.2, random_state=42)
+
 svm_model = SVC(kernel='linear', C=1.0)
 
 svm_model.fit(X_train, y_train)
@@ -14,6 +17,9 @@ svm_model.fit(X_train, y_train)
 joblib.dump(svm_model, 'svm_model.pkl')
 
 y_pred = svm_model.predict(X_test)
+
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
+vocabulary_used_during_training = joblib.load('tfidf_vocabulary.pkl')
 
 print('\nClassification Report:')
 print(classification_report(y_test, y_pred))
